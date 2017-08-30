337c6f630e050a289c3d41eb4bbf127a9c23850e
Povilas Simanaitis <povsmn@telesoftas.com>
2017-08-30 21:50:36 +0300

    Used angular started instead of ng cli generated skeleton.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,134 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { AppComponent } from './app.component';
-import { ROUTES } from './app.routes';
-import { CatalogModule } from './catalog/catalog.module';
-import { MdToolbarModule, MdButtonModule, MdProgressBarModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UpdateCatalogComponent } from './catalog/update-catalog/update-catalog.component';
-
-import { Store, StoreModule } from '@ngrx/store';
-import { reducers } from './reducers';
-import { CatalogEffect } from './catalog/load-catalog-data/load-catalog-data.effects';
-import { EffectsModule } from '@ngrx/effects';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  NgModule,
+  ApplicationRef
+} from '@angular/core';
+import {
+  removeNgStyles,
+  createNewHosts,
+  createInputTransfer
+} from '@angularclass/hmr';
+import {
+  RouterModule,
+  PreloadAllModules
+} from '@angular/router';
 
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+import { ENV_PROVIDERS } from './environment';
+import { ROUTES } from './app.routes';
+// App is our top level component
+import { AppComponent } from './app.component';
+import { APP_RESOLVER_PROVIDERS } from './app.resolver';
+import { AppState, InternalStateType } from './app.service';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { NoContentComponent } from './no-content';
+import { XLargeDirective } from './home/x-large';
+
+import '../styles/styles.scss';
+import '../styles/headings.css';
+
+// Application wide providers
+const APP_PROVIDERS = [
+  ...APP_RESOLVER_PROVIDERS,
+  AppState
+];
+
+type StoreType = {
+  state: InternalStateType,
+  restoreInputValues: () => void,
+  disposeOldHosts: () => void
+};
+
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
 @NgModule({
+  bootstrap: [ AppComponent ],
   declarations: [
     AppComponent,
-    UpdateCatalogComponent
+    AboutComponent,
+    HomeComponent,
+    NoContentComponent,
+    XLargeDirective
   ],
+  /**
+   * Import Angular's modules.
+   */
   imports: [
     BrowserModule,
-    CatalogModule,
-    RouterModule.forRoot(ROUTES, { useHash: false, preloadingStrategy: PreloadAllModules }),
-    MdToolbarModule,
-    MdButtonModule,
-    BrowserAnimationsModule,
-    StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([CatalogEffect]),
-    HttpModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
   ],
+  /**
+   * Expose our Services and Providers into Angular's dependency injection.
+   */
   providers: [
-   [
-     class DataResolver implements Resolve<any> {
-       public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {}
-     }
-   ]
-  ],
-  bootstrap: [AppComponent]
+    ENV_PROVIDERS,
+    APP_PROVIDERS
+  ]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(
+    public appRef: ApplicationRef,
+    public appState: AppState
+  ) {}
+
+  public hmrOnInit(store: StoreType) {
+    if (!store || !store.state) {
+      return;
+    }
+    console.log('HMR store', JSON.stringify(store, null, 2));
+    /**
+     * Set state
+     */
+    this.appState._state = store.state;
+    /**
+     * Set input values
+     */
+    if ('restoreInputValues' in store) {
+      let restoreInputValues = store.restoreInputValues;
+      setTimeout(restoreInputValues);
+    }
+
+    this.appRef.tick();
+    delete store.state;
+    delete store.restoreInputValues;
+  }
+
+  public hmrOnDestroy(store: StoreType) {
+    const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
+    /**
+     * Save state
+     */
+    const state = this.appState._state;
+    store.state = state;
+    /**
+     * Recreate root elements
+     */
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    /**
+     * Save input values
+     */
+    store.restoreInputValues  = createInputTransfer();
+    /**
+     * Remove styles
+     */
+    removeNgStyles();
+  }
+
+  public hmrAfterDestroy(store: StoreType) {
+    /**
+     * Display new elements
+     */
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,15 @@
 import { Routes } from '@angular/router';
-import { CATALOG_ROUTES } from './catalog/catalog.routes';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { NoContentComponent } from './no-content';
+
+import { DataResolver } from './app.resolver';
+
 export const ROUTES: Routes = [
-    ...CATALOG_ROUTES,
+  { path: '',      component: HomeComponent },
+  { path: 'home',  component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'detail', loadChildren: './+detail#DetailModule'},
+  { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
+  { path: '**',    component: NoContentComponent },
 ];
